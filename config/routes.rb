838d1f14fef5f18eadbf5c2af8c1719864c944d0
飯田恭子 <iidakyouko@mba.local>
2020-05-26 18:35:46 +0900

    ユーザー認証:コントローラー作成、ルート修正、bcrypt導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'mypage', to: "users#me"
+  post "login", to: 'sessions#create'
+  delete "logout", to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
@@ -8,6 +12,7 @@ Rails.application.routes.draw do
   # get 'boards/:id',to:"boards#show"
 
   root "boards#index"
+  resources :users,only: %i[new create]
   resources :boards
   resources :comments,only: %i[create destroy]
 end
